8f36b05c66701e4fcc66399a960e80c58d84d3d1
Marco Tello <muchaweb@gmail.com>
2016-12-14 16:00:55 -0600

    adding emit messages and broadcast to the rest of the connected apps

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,7 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to socket io');
 });
+
+socket.on('message', function (message) {
+  document.getElementsByClassName("pagetitle")[0].innerHTML = message.text;
+});
